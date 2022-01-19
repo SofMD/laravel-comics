3bde61a367e412a3800223027e9c25dcf289208f
sofMD <xsofiamaria.32@gmail.com>
2022-01-19 15:45:09 +0100

    setup

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="container">
+        {{-- left --}}
+        <button>SIGB-UP NOW!</button>
+
+        {{-- RIGHT --}}
+        <div class="footerright">
+            
+        </div>
+    </div>
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+
+    <div class="container">
+        {{-- logo --}}
+        <img src="{{ asset('images/dc-logo.png')}}" alt="logo">
+
+        {{-- navigazione --}}
+        <div class="nav">
+            <a href="/">COMICS</a>
+            <a href="/">NEWS</a>
+        </div>
+    </div>
+
+
+</header>
