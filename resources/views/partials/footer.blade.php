876be0bb2d27ba3e1eab2f4e61226b2009d7645c
sofMD <xsofiamaria.32@gmail.com>
2022-01-19 17:21:09 +0100

    header e footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,42 @@
 <footer>
-    <div class="container">
+    <div class="container d-fl sp-bw ">
         {{-- left --}}
-        <button>SIGB-UP NOW!</button>
+        <button>SIGN-UP NOW!</button>
 
         {{-- RIGHT --}}
-        <div class="footerright">
-            
+        <div class="footerright d-fl">
+            <h3>FOLLOW US</h3>
+
+            <!-- lista icone -->
+            <ul class=" d-fl li-st">
+                <li class="c-pointer">
+                    <a href="/">
+                        <img src="{{ asset('images/footer-facebook.png')}}" alt="">
+                    </a>
+                </li>
+                <li class="c-pointer">
+                    <a href="/">
+                        <img src="{{ asset('images/footer-twitter.png')}}" alt="">
+                    </a>
+                </li>
+                <li class="c-pointer">
+                    <a href="/">
+                        <img src="{{ asset('images/footer-youtube.png')}}" alt="">
+                    </a>
+                </li>
+                <li class="c-pointer">
+                   <a href="/">
+                    <img src="{{ asset('images/footer-pinterest.png')}}" alt="">
+                   </a>
+                </li>
+                <li class="c-pointer">
+                    <a href="/">
+                        <img src="{{ asset('images/footer-periscope.png')}}" alt="">
+                    </a>
+                </li>
+                
+            </ul>
+
         </div>
     </div>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,17 @@
 <header>
 
-    <div class="container">
+    <div class="container d-fl">
         {{-- logo --}}
         <img src="{{ asset('images/dc-logo.png')}}" alt="logo">
 
         {{-- navigazione --}}
         <div class="nav">
-            <a href="{{ route('home') }}">COMICS</a>
-            <a href="{{ route('news') }}">NEWS</a>
+            <a href="{{ route('home') }}"
+            @if (Request::route()->getName() === 'home') class="active" @endif
+            >COMICS</a>
+            <a href="{{ route('news') }}"
+            @if (Request::route()->getName() === 'news') class="active" @endif
+            >NEWS</a>
         </div>
     </div>
 
