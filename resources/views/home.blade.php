b3c70eaf02e07a13d175d7fe68bff893bef79cde
sofMD <xsofiamaria.32@gmail.com>
2022-01-19 18:14:58 +0100

    main - card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.main')
 
 @section('main-content')
-    ghfh
+
+<section>
+    <div class="container">
+        <ul class="cards">
+            @foreach ($comics as $comic)
+            <li class="li-st">
+                <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
+                <div class="text">{{$comic['series']}}</div>
+            </li>
+                
+            @endforeach
+            
+        </ul>
+
+        {{-- bottone --}}
+        <div class="btn-container">
+            <button class=" main-button">load more</button>
+        </div>
+
+
+    </div>
+</section>
+
+
+    
 @endsection
